3eaa92fb27610e3f604e9416de9baf1d64becbbb
Praphulla Ghimire <103087117+gpraphul99@users.noreply.github.com>
2022-10-19 07:58:19 +0545

    New file created praphulla.js

---
+++ praphulla.js
@@ -0,0 +1,14 @@
+let cars = [
+    {
+      "color": "purple",
+      "type": "minivan",
+      "registration": new Date('2017-01-03'),
+      "capacity": 7
+    },
+    {
+      "color": "red",
+      "type": "station wagon",
+      "registration": new Date('2018-03-03'),
+      "capacity": 5
+    }
+]
